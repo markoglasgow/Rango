10152b847daaeda25626d7920535ee5be1473de0
Marko Caklovic <2349514C@student.gla.ac.uk>
2018-01-22 22:25:04 +0000

    Added exercise migrations

--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,11 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['title', 'url']
+    list_display = ('title', 'url')
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
