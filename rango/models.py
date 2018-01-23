aa373769afa2c4cee0d8e15e9f53ae538ae1e8b7
Marko Caklovic <2349514C@student.gla.ac.uk>
2018-01-23 16:38:48 +0000

    Refactoring for Chapter 7 exercise

--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+MAX_CHAR_FIELD_LENGTH = 128
+
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(blank=True, unique=True)
@@ -22,7 +24,7 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
